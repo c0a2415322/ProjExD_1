165d092e4d7b3c75e8521a542b94fe20bb57ecef
Daichi <c0a2415322@edu.teu.ac.jp>
2025-11-18 16:49:28 +0900

    練習8：背景反転

--- pygame_image.py
+++ pygame_image.py
@@ -11,6 +11,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     tori = pg.image.load("fig/3.png")
     tori = pg.transform.flip(tori,True,False)
     tmr = 0
@@ -23,7 +24,7 @@ def main():
 
         x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(tori,[300,200])
         pg.display.update()
         tmr += 1        
