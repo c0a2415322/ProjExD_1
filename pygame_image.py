d34cc28f58595b7c8d0377d6d3f7fc86ce612dc6
Daichi <c0a2415322@edu.teu.ac.jp>
2025-11-18 18:10:34 +0900

    演習１：流れていくこうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -23,13 +23,16 @@ def main():
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT: 
+                return
 
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
+        kk_rct.move_ip((-1, 0))
+        
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
@@ -38,12 +41,11 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
         screen.blit(tori,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-
 
 
 
