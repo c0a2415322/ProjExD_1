fdad482a7506e154a619c85ed83e5fe486c247bb
Daichi <c0a2415322@edu.teu.ac.jp>
2025-11-18 17:22:03 +0900

    練習10：移動操作

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,9 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)
     tori = pg.image.load("fig/3.png")
     tori = pg.transform.flip(tori,True,False)
+    kk_rct = tori.get_rect()
+    kk_rct.center = 300, 200
+    screen.blit(tori, kk_rct)
     tmr = 0
 
     
@@ -22,11 +25,21 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(tori,[300,200])
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0))
+        screen.blit(tori,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
